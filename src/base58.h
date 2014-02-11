5f35fdbce8ad7fbc1f2330655ff5cec8f31de231
Hank Booty <hank@bootycoin.org>
2014-02-11 01:30:03 -0800

    more customizations, more booty

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // BootyCoin addresses start with L
+        PUBKEY_ADDRESS = 25, // BootyCoin addresses start with B
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
